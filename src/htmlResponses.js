45bb963514ff5f0baeee59d4721205ee702eef47
JLHGD <jlhgamedev@gmail.com>
2021-09-12 13:57:57 -0400

    Actually completed assignment I think

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,11 +1,27 @@
 const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
-const getIndex = (request, response) => {
+const loadHTML = (request, response, file) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+  response.write(file);
   response.end();
 };
 
+const getIndex = (request, response) => {
+  loadHTML(request, response, index);
+};
+
+const getPage2 = (request, response) => {
+  loadHTML(request, response, page2);
+};
+
+const getPage3 = (request, response) => {
+  loadHTML(request, response, page3);
+};
+
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
